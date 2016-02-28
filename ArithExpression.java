e036703ba02897db629a00501918eb6553f5b78c
ElTruquini <d_olaya@hotmail.com>
2016-02-27 22:36:30 -0800

    Final corrections made

--- ArithExpression.java
+++ ArithExpression.java
@@ -8,6 +8,9 @@
  * Details: CSC115 Assignment 3 Calculator
 */
 import java.util.regex.*;
+import java.util.*;
+import java.lang.Math.*;
+
 
 public class ArithExpression {
 
@@ -39,7 +42,7 @@ public class ArithExpression {
 	 * The method makes use of Java Pattern matching and Regular expressions to
 	 * isolate the operators and parentheses.
 	 * The operands are assumed to be the substrings delimited by the operators and parentheses.
-	 * The result is captured in the infixToken list, where each token is 
+	 * The result is captured in the infixTokens list, where each token is 
 	 * an operator, a paren or a operand.
 	 * @param express The string that is assumed to be an arithmetic expression.
 	 */
@@ -115,63 +118,155 @@ public class ArithExpression {
 	 * the Exception is propogated through the constructor.
 	 */
 	private void infixToPostfix() {
-		String postFixExp = "";
-		System.out.println(infixTokens);
+		postfixTokens  = new TokenList(infixTokens.toString().length());
 		String token = "";
 		StringStack stacky = new StringStack ();
+		//2.4 -3 5 + *
+		//System.out.println("To convert: " + infixTokens.toString() );
 
-		for (int i = 0; i < infixTokens.toString().length(); i++){
-			token = "" + infixTokens.toString().charAt(i);
-			
-			//System.out.println(token);
-			switch (token){
-				case "1": case "2": case "3": case "4": case "5": case "6":
-				case "7": case "8": case "9":
-					postFixExp = postFixExp + token;
-					break;
+		Scanner reader = new Scanner (infixTokens.toString());
+		reader.useDelimiter("\\s");
 
-				case "(":
-					stacky.push(token);
-					break;
-
-				case ")":
-					while(!stacky.peek().equals("(")){
-						postFixExp = postFixExp + stacky.pop();
-					}
-					String openParen = stacky.pop();
-					break;
-
-				case "+": case "-": case "*": case "/": case "^":  
-					while (!stacky.isEmpty() && !stacky.peek().equals("(") ){
-						// need to include prescedence
-						postFixExp = postFixExp + stacky.pop();
-					}
-					stacky.push(token);
-					break;
+		while (reader.hasNext()){
+			token = ""+reader.next();
+			//System.out.println("**********TOKEN TO PROCESS: " + token );
+			//looking for Operands
+			if (!isOperator(token) && !token.equals("(") && !token.equals(")")){
+				//System.out.println("I made IF 1 !isOperator token :"+ token + " prevToken :" + prevToken);;
+				postfixTokens.append(token);
 			}
+			if (token.equals("(")){ 
+				//System.out.println("I made IF 2 .equals'(' token :"+ token + " prevToken :" + prevToken);
+				stacky.push(token);
+			}
+			if (token.equals(")")){
+				//System.out.println("I made IF 3 .equals')' token :"+ token + " prevToken :" + prevToken);
+				while(!stacky.peek().equals("(")){
+					postfixTokens.append(stacky.pop());
+				}
+				String openParen = stacky.pop();
+			}
+
+			if (isOperator(token)){
+				//System.out.println("I made IF 4 isOperator token :"+ token + " prevToken :" + prevToken);
+				while ( !stacky.isEmpty() && !stacky.peek().equals("(") 
+					&& precedence(token) <= precedence (stacky.peek()) ){
+					postfixTokens.append(stacky.pop());
+				}
+				stacky.push(token);
+			}
+		
+		//System.out.println("===STRING: " + postfixTokens.toString() );	
 		}
 		while (!stacky.isEmpty()){
-			postFixExp = postFixExp + stacky.pop();
+			postfixTokens.append(stacky.pop());
 		}
-		System.out.println(postFixExp);
-		
+		//System.out.println("from method " + postfixTokens.toString());
 	
 	}
-
+	
+	public int precedence (String token){
+		if (token == "^"){
+			return 5;
+		}
+		if ((token == "*") || (token == "/")){
+			return 4;
+		}
+		if ((token == "+") || (token == "-")){
+			return 3;
+		}
+		if (token == "(") {
+			return 2;
+		}
+		if (token == ")"){
+			return 1;
+		}
+		return 0;
+	}
 	public String getInfixExpression() {
-		return "";
+		return infixTokens.toString();
 	}
 
 	public String getPostfixExpression() {
-	 	return "";
+	 	return postfixTokens.toString();
 	}
 		
 	public double evaluate() {
-		return 9.0;
+		StringStack operandStack = new StringStack ();
+		Scanner reader = new Scanner (postfixTokens.toString());
+		reader.useDelimiter("\\s");
+		/*
+		String dom ="Daniel   x  Olaya Moran";
+		Scanner testing = new Scanner (dom);
+		while (testing.hasNext()){
+			System.out.println(testing.next());
+		}
+		return 2.0;
 	}
-						
+	*/
+		System.out.println("Evaluate method with string: " + postfixTokens.toString());
+		int count =0;
+		double operand1, operand2, result = 0;
+		while (reader.hasNext()){
+			String token = reader.next();
+			System.out.println("token processed: " + token);
+			if (token.equals(" ")){
+				String holder = token;
+			}
+			if (!isOperator(token) && !token.equals(" ")){
+				operandStack.push(token);	
+				System.out.println("Pushing token: " + token);
+			}
+			if (isOperator(token) && !token.equals(" ")){
+				System.out.println("peeking!: " + operandStack.peek());
+				operand1 = Double.parseDouble(operandStack.pop());
+				System.out.println("peeking!: " + operandStack.peek());
+
+				operand2 = Double.parseDouble(operandStack.pop());
+				System.out.println("operand1: " + operand1 + "operand2: " + operand2);
+				switch (token){
+					case "+": 
+						result = operand1 + operand2;
+						break;
+					case "-": 
+						result = operand1 - operand2;
+						break;
+					case "*": 
+						result = operand1 * operand2;
+						break;
+					case "/": 
+						result = operand1 / operand2;
+						break;
+					case "^": 
+						result = Math.pow(operand1, operand2);
+						break;
+				}
+			System.out.println("Pushing result: " + result);
+			String resultString = ""+result;
+			operandStack.push(resultString);
+			}
+		}
+
+		return result;
+	}
+	//Internal method for testing		
 	public static void main(String[] args) {
-		ArithExpression test = new ArithExpression("2+2*3+3");
+		//ArithExpression test = new ArithExpression("2 * ( -3 + 5 )");// Result:4 - Correct!
+		//ArithExpression test = new ArithExpression("6 +1 +9 +(10 *2)");// Result:36 - Correct!
+		//ArithExpression test = new ArithExpression("2/ 1");
+		//ArithExpression test = new ArithExpression("(2 /2) +1");
+		//ArithExpression test = new ArithExpression("-5+3+(6*2)");
+		//ArithExpression test = new ArithExpression("15*10-2");
+		ArithExpression test = new ArithExpression("6 +1 +9 +(10 *2)");
+		//ArithExpression test = new ArithExpression("6 +1 +9 +(10 *2)");
+		//ArithExpression test = new ArithExpression("6 +1 +9 +(10 *2)");
+
+
+		test.getInfixExpression();
+		test.getPostfixExpression();
+
+		System.out.println("Evaluate = " + test.evaluate());
+		
 	}
 			
 }
